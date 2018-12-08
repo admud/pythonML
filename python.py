87bddb5a2c728c2e89a6541c3822519fc8a8e353
muhdahmaddahlan <mudmud.muhd@gmail.com>
2018-12-08 11:05:29 +0800

    ml practice part 3

--- python.py
+++ python.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import  quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -15,4 +16,17 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.
 # dataframe for columns that we really want to see
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
 
+# change to what you want to predict/forecast
+forecast_col = 'Adj. Close'
+
+# fill in missing data, as the algo cannot take in not a number
+df.fillna(-99999, inplace =True)
+
+# rounds up the value, using data from 10 days out?
+forecast_out = int(math.ceil(0.01*len(df)))
+
+# shifting column up
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+df.dropna(inplace = True)
 print(df.head())
